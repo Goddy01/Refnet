c1229c1821a7cc44c0ae42756e0df6f74db320d2
Goddy01 <adigungodwin2@gmail.com>
2024-09-23 02:56:24 +0100

    updated

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -2,9 +2,10 @@ import React from 'react'
 
 export default function Footer() {
   return (
-    <footer className="bg-gradient-to-r from-blue-500 to-[#19469D] text-white text-center py-4 font-semibold">
+    <footer className="bg-gradient-to-r from-blue-500 to-[#19469D] text-white text-center py-4">
       <p className="text-sm">
-        &copy; {new Date().getFullYear()} Built by <a href='https://toshirothe3rd.vercel.app/' className="font-bold hover:text-blue-300">Toshiro</a>
+        &copy; {new Date().getFullYear()} Refnet. All rights reserved. 
+        {/* <a href='https://toshirothe3rd.vercel.app/' className="font-bold hover:text-blue-300">Toshiro</a> */}
       </p>
     </footer>
   )
