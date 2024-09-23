b8cb94a0b1e25a8b3b2e520560901ec1fd9717a3
Goddy01 <adigungodwin2@gmail.com>
2024-09-23 02:44:02 +0100

    updated Footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 export default function Footer() {
   return (
-    <footer className="bg-[#19469D] text-white text-center py-4">
+    <footer className="bg-gradient-to-r from-blue-500 to-[#19469D] text-white text-center py-4 font-semibold">
       <p className="text-sm">
         &copy; {new Date().getFullYear()} Built by <a href='https://toshirothe3rd.vercel.app/' className="font-bold hover:text-blue-300">Toshiro</a>
       </p>
